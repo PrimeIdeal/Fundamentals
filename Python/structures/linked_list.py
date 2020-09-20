4e68fcfc814e79bfbcc3cb1393f09b4a06edbfef
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-09-20 19:39:04 -0400

    new constructor and serialize function for linked_list

--- Python/structures/linked_list.py
+++ Python/structures/linked_list.py
@@ -1,219 +1,63 @@
-class node:
-    """
-    A simple node that contains a value and a reference to another node.
-    """
-
-    def __init__(self, val=None):
-        """
-        Constructor for node class.
-        """
-        self.val = val
-        self.next = None
-
-    def __eq__(self, other_node):
-        """
-        Returns True if other_node is equal to self, False otherwise.
-
-        Define node equality as equal node values and equal values in the
-        respective next nodes.
-        """
-        if (self.next and not other_node.next) or (not self.next and other_node.next):
-            return False
-        
-        equal = (self.val == other_node.val)
-        if equal and self.next:
-            equal = equal and (self.next.val == other_node.next.val)
-        
-        return equal
-
-
-class double_node(node):
-    """
-    An extension of the simple node that references a Previous node and a Next node.
-    """
-
-    def __init__(self, val=None):
-        """
-        Constructor for double_node class.
-        """
-        node.__init__(self, val)
-        self.prev = None
-
-    def __eq__(self, other_node):
-        """
-        Returns True if other_node is equal to self, False otherwise.
-
-        Define double_node equality as equal double_node values and equal values in the
-        respective prev and next double_nodes.
-        """
-        if ((self.next and not other_node.next) \
-            or (not self.next and other_node.next) \
-            or (self.prev and not other_node.prev) \
-            or (not self.prev and other_node.prev)):
-            return False
-        
-        equal = (self.val == other_node.val)
-        if equal and self.next:
-            equal = equal and (self.next.val == other_node.next.val)
-        if equal and self.prev:
-            equal = equal and (self.prev.val == other_node.prev.val)
-        
-        return equal
-
-    def link_next(self, other_node):
-        """
-        Links self to its next double_node.
-        """
-        if other_node:
-            self.next, other_node.prev = other_node, self
-
-    def link_prev(self, other_node):
-        """
-        Links self to its prev double_node.
-        """
-        if other_node:
-            self.prev, other_node.next = other_node, self
-
 class linked_list:
-    
-    def __init__(self, val=None):
-        """
-        Constructor for linked_list class.
-        """
-        if val is not None:
-            self.head = node(val)
-        else:
-            self.head = val
+    """
+    A simple linked list where each node contains a value and a
+    reference to the next node in the list.
+    """
 
-    def get_tail(self):
+    def __init__(self, node_list=None):
         """
-        Returns the tail node of the linked_list.
+        Constructor for the linked_list class.
         """
-        tail = self.head
+        self.val = node_list[0] if node_list else None
+        self.next = linked_list(node_list=node_list[1:]) \
+            if node_list[1:] else None
 
-        if tail:
-            while tail.next:
-                tail = tail.next
-
-        return tail
-
-    def insert_head(self, new_val):
+    def __str__(self):
         """
-        Inserts a new node at the head of the linked_list.
+        Returns the linked list's unique string representation.
         """
-        new_node = node(new_val)
-        self.head, new_node.next = new_node, self.head
+        str_representation = '['
 
-    def insert_tail(self, new_val):
+        str_representation += '->'.join(str(elt) for elt in self.serialize())
+
+        return str_representation + ']'
+
+    def __eq__(self, other_list):
         """
-        Inserts a new node at the tail of the linked_list.
+        Returns True if other_list is equal to self, False otherwise.
         """
-        new_node = node(new_val)
-        tail = self.get_tail()
-        tail.next = new_node
+        return self.serialize() == other_list.serialize()
 
-    def remove_head(self):
+    def serialize(self):
         """
-        Removes and returns the node at the head of the linked_list.
+        Returns the linked list's unique array representation.
         """
-        old_head = self.head
-        if self.head:
-            self.head = old_head.next
-        old_head.next = None
-        return old_head
+        curr = self
+        list_representation = []
 
-    def remove_tail(self):
-        """
-        Removes and returns the node at the tail of the linked_list.
-        """
-        curr = self.head
-
-        if not curr or not curr.next:
-            self.head = None
-            return curr
-
-        while curr.next.next:
-            curr = curr.next
-        
-        old_tail = curr.next
-        curr.next = None
-
-        return old_tail
-    
-class double_linked_list:
-
-    def __init__(self, val=None):
-        """
-        Constructor for double_linked_list class.
-        """
-        if val is not None:
-            self.head = double_node(val)
-        else:
-            self.head = val
-
-    def get_tail(self):
-        """
-        Returns the tail double_node of the double_linked_list.
-        """
-        tail = self.head
-
-        if tail:
-            while tail.next:
-                tail = tail.next
-        
-        return tail
-
-    def insert_head(self, val):
-        """
-        Inserts a new double_node at the head of the double_linked_list.
-        """
-        new_head = double_node(val)
-        old_head = self.head
-
-        if old_head:
-            old_head.link_prev(new_head)
-        self.head = new_head
-
-    def insert_tail(self, val):
-        """
-        Inserts a new double_node at the tail of the double_linked_list.
-        """
-        new_node = double_node(val)
-        tail = self.get_tail()
-
-        if not tail:
-            self.head = new_node
-        else:
-            tail.link_next(new_node)
-
-    def remove_head(self):
-        """
-        Removes and returns the double_node at the head of the double_linked_list.
-        """
-        old_head = self.head
-
-        if not old_head or not old_head.next:
-            self.head = None
-        else:
-            old_head.next.prev = None
-            self.head = old_head.next
-            old_head.next = None
-
-        return old_head
-
-    def remove_tail(self):
-        """
-        Removes and returns the double_node at the tail of the double_linked_list.
-        """
-        curr = self.head
-
-        if not curr or not curr.next:
-            self.head = None
-            return curr
-
-        while curr.next.next:
+        while curr:
+            list_representation.append(curr.val)
             curr = curr.next
 
-        old_tail = curr.next
-        curr.next, old_tail.prev = None, None
-        return old_tail
+        return list_representation
+
+    def get_node(self, val):
+        pass
+
+    def get_tail(self):
+        pass
+
+    def insert_head(self):
+        pass
+
+    def insert_tail(self):
+        pass
+
+    def remove_head(self):
+        pass
+
+    def remove_tail(self):
+        pass
+
+    def delete_node(self, val):
+        pass
--- Python/tests/test_linked_list.py
+++ Python/tests/test_linked_list.py
@@ -1,104 +1,23 @@
-import unittest
+import pytest
 
-from structures.linked_list import (
-    double_linked_list,
-    double_node,
-    linked_list,
-    node
-)
+from Python.structures.linked_list import linked_list
 
 
-class test_linked_list(unittest.TestCase):
+class TestLinkedList:
+    """
+    Test class for the linked_list class.
+    """
 
-    def test_get_tail(self):
-        l = linked_list(7)
-        l.insert_head(8)
-        m = linked_list(19)
-        n = linked_list(27)
-        n.remove_tail()
+    @pytest.mark.parametrize(
+        'node_list, expected',
+        [
+            ([1, 2, 3], [1, 2, 3]),
+            ([None], [None]),
+            ([None, 3], [None, 3]),
+            ([], [None])
+        ]
+    )
+    def test_serialize(self, node_list, expected):
+        test_list = linked_list(node_list=node_list)
 
-        self.assertEqual(l.get_tail(), node(7))
-        self.assertEqual(m.get_tail(), node(19))
-        self.assertEqual(n.get_tail(), None)
-
-    def test_insert_head(self):
-        a, b = node(7), node(8)
-        b.next = a
-        l = linked_list(7)
-
-        self.assertEqual(l.head, a)
-
-        l.insert_head(8)
-
-        self.assertEqual(l.head, b)
-
-    def test_insert_tail(self):
-        l = linked_list(7)
-        l.insert_tail(8)
-        l.insert_tail(9)
-        
-        self.assertEqual(l.get_tail(), node(9))
-
-    def test_remove_head(self):
-        l = linked_list(7)
-        l.insert_tail(8)
-
-        self.assertEqual(l.remove_head(), node(7))
-        self.assertEqual(l.head, node(8))
-
-    def test_remove_tail(self):
-        l = linked_list(7)
-        l.insert_head(8)
-
-        self.assertEqual(l.remove_tail(), node(7))
-        self.assertEqual(l.get_tail(), node(8))
-
-
-class test_double_linked_list(unittest.TestCase):
-
-    def test_get_tail(self):
-        l = double_linked_list(7)
-        l.insert_head(8)
-        a, b = double_node(7), double_node(8)
-        a.link_prev(b)
-        m = double_linked_list(19)
-        n = double_linked_list(27)
-        n.remove_head()
-
-        self.assertEqual(l.get_tail(), a)
-        self.assertEqual(m.get_tail(), double_node(19))
-        self.assertEqual(n.get_tail(), None)
-
-    def test_insert_head(self):
-        l = double_linked_list(7)
-        l.insert_head(8)
-        a, b = double_node(7), double_node(8)
-        b.link_next(a)
-
-        self.assertEqual(l.head, b)
-
-    def test_insert_tail(self):
-        l = double_linked_list(7)
-        l.insert_tail(8)
-        a, b = double_node(7), double_node(8)
-        b.link_prev(a)
-
-        self.assertEqual(l.get_tail(), b)
-
-    def test_remove_head(self):
-        l = double_linked_list(7)
-        l.insert_tail(8)
-
-        self.assertEqual(l.remove_head(), double_node(7))
-        self.assertEqual(l.head, double_node(8))
-    
-    def test_remove_tail(self):
-        l = double_linked_list(7)
-        l.insert_head(8)
-
-        self.assertEqual(l.remove_tail(), double_node(7))
-        self.assertEqual(l.get_tail(), double_node(8))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert test_list.serialize() == expected
