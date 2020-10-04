0ae06a9246fd4c5de7d51147c2891e0598505b2e
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-10-04 17:26:32 -0400

    updated docstrings and typing

--- python/structures/linked_list.py
+++ python/structures/linked_list.py
@@ -12,24 +12,45 @@ class linked_list:
     reference to the next node in the list.
     """
 
-    def __init__(self, node_list=None):
+    def __init__(self, node_list: List[Any] = None):
         """
         Constructor for the linked_list class.
+
+        Parameters
+        ----------
+        node_list : List[Any]
+            Optional: Ordered list of values to be inserted into the doubly
+            linked list.
         """
         self.empty = False if node_list else True
         self.val = node_list[0] if node_list else None
         self.next = linked_list(node_list=node_list[1:]) \
             if node_list and node_list[1:] else None
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Returns the linked list's unique string representation.
+
+        Returns
+        -------
+        str
+            String representation of the linked_list object.
         """
         return '['+'->'.join(str(elt) for elt in self.serialize())+']'
 
-    def __eq__(self, other_list):
+    def __eq__(self, other_list: linked_list) -> bool:
         """
-        Returns True if other_list is equal to self, False otherwise.
+        Determines if self is equal to other_list.
+
+        Parameters
+        ----------
+        other_list : linked_list
+            Linked list to compare self to.
+
+        Returns
+        -------
+        bool
+            True if other_list is equal to self, False otherwise.
         """
         curr1, curr2 = self, other_list
         while curr1 and curr2:
@@ -39,9 +60,14 @@ class linked_list:
 
         return not (curr1 or curr2)
 
-    def serialize(self):
+    def serialize(self) -> List[Any]:
         """
         Returns the linked list's unique array representation.
+
+        Returns
+        -------
+        List[Any]
+            Array representation of the linked_list object.
         """
         curr = self
         list_representation = []
@@ -53,10 +79,20 @@ class linked_list:
 
         return list_representation
 
-    def get_node(self, val):
+    def get_node(self, val: Any) -> linked_list:
         """
         Returns the first node containing a given value if it exists
         in the list, None otherwise.
+
+        Parameters
+        ----------
+        val : Any
+            Node value to search for.
+
+        Returns
+        -------
+        linked_list
+            The node containing the value.
         """
         curr = self
 
@@ -67,9 +103,14 @@ class linked_list:
 
         return None
 
-    def get_tail(self):
+    def get_tail(self) -> linked_list:
         """
         Returns the tail node of the linked list.
+
+        Returns
+        -------
+        linked_list
+            The final node of the linked_list object.
         """
         curr = self
 
@@ -78,10 +119,20 @@ class linked_list:
 
         return curr
 
-    def insert_head(self, val):
+    def insert_head(self, val: Any) -> linked_list:
         """
         Returns a new linked list with a node containing the given value at
         its head.
+
+        Parameters
+        ----------
+        val : Any
+            Node value to be inserted.
+
+        Returns
+        -------
+        linked_list
+            The list with the new node at its head.
         """
         if self.empty:
             self.val, self.empty = val, False
@@ -91,10 +142,15 @@ class linked_list:
             new_list.next = self
             return new_list
 
-    def insert_tail(self, val):
+    def insert_tail(self, val: Any):
         """
         Appends a new node containing the given value at the tail of the
         linked list.
+
+        Parameters
+        ----------
+        val : Any
+            Node value to be inserted.
         """
         if self.empty:
             self.val, self.empty = val, False
@@ -125,9 +181,14 @@ class linked_list:
                     curr = curr.next
                 curr.next = None
 
-    def delete_node(self, val):
+    def delete_node(self, val: Any):
         """
         Removes the first node containing the given value.
+
+        Parameters
+        ----------
+        val : Any
+            Value to search for.
         """
         if self.val == val:
             if not self.next:
@@ -143,9 +204,20 @@ class linked_list:
                 break
             curr = curr.next
 
-    def reverse(self, recursive=True):
+    def reverse(self, recursive=True) -> linked_list:
         """
         Reverses the linked list.
+
+        Parameters
+        ----------
+        recursive : bool
+            Optional: Indicates whether to use recursive or iterative
+            implementation (recursive by default).
+
+        Returns
+        -------
+        linked_list
+            The list in reverse order.
         """
         curr = self
 
