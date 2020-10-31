d7787811a963fd6d6615caa65cfaead7b7eb2cc2
Pierre Gauvreau <pierre.n.gauvreau@gmail.com>
2020-10-31 14:25:06 -0600

    added test_add_content

--- python/tests/test_file_system.py
+++ python/tests/test_file_system.py
@@ -29,4 +29,27 @@ class TestFileSystem:
     )
     def test_mkdir(self, system, path, ls_path, expected):
         system.mkdir(path)
+
         assert system.ls(ls_path) == expected
+
+    @pytest.mark.parametrize(
+        'path, content, ls_path, expected',
+        [
+            ('/a', 'A', '/', (['a', 'b', 'c'], 'A')),
+            ('/c/f', 'F', '/c', (['f'], 'F')),
+            ('/d/g', 'G', '/d', (['g'], 'G')),
+            ('/a', '+', '/', (['a', 'b', 'c', 'd'], 'A+'))
+        ],
+        ids=[
+            'New file in root dir',
+            'New file in existing folder',
+            'New file in new folder',
+            'Appending content to existing file'
+        ]
+    )
+    def test_add_content(self, system, path, content, ls_path, expected):
+        system.add_content(path, content)
+        expected_ls, expected_cat = expected
+
+        assert system.ls(ls_path) == expected_ls
+        assert system.cat(path) == expected_cat
